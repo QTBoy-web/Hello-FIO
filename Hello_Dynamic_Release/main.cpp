d784ebe6a52e4ef5eadd5be2512719323600fa0e
Tofi-DES <prosanovviktor@gmail.com>
2026-06-10 19:45:21 +0300

    Update main.cpp

--- Hello_Dynamic_Release/main.cpp
+++ Hello_Dynamic_Release/main.cpp
@@ -6,5 +6,6 @@ int main()
 {
     cout << "Hello, Oleg Fomin Victorovich!" << endl;
     cout << "Hello, Chernov Nikita Alexandrovich" << endl;
+    cout << "Hello, Prosyanov Viktor Nikolaevich" << endl;
     return 0;
 }
